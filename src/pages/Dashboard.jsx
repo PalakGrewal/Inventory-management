17155b112744b84355a046ed60b55c9360042e4f
Yatharth2609 <yatharth.mishra2002@gmail.com>
2023-11-26 21:34:44 +0530

    ds

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Sidebar from '../components/Sidebar'
-import Header from '../components/Header'
-import { Outlet } from 'react-router'
 
 const Dashboard = () => {
   return (
