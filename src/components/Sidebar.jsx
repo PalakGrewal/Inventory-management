29d55c77d9f242d2c09e57f37ac7a50d9bf83bc8
MrtitaniumJ <jkjatinsharma72@gmail.com>
2023-11-26 22:42:38 +0530

    Sidebar set

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -37,45 +37,38 @@ const Sidebar = ({ children }) => {
   ];
 
   return (
-    <div
-      className="flex
-     h-[90%] text-4xl float-left mr-4"
-    >
-      <div className="sidebar flex sticky flex-col text-white bg-gray-700 mt-2 ml-2 mb-2 rounded text-4xl ">
-        <div className="bars pt-4  pb-2 px-4 ">
-          <div className="flex">
-            <div
-              className={`sidebar flex flex-col gap-3 text-white bg-gray-700 ${isOpen ? "w-48" : "w-16"
-                } mt-2 ml-2 mb-2 rounded text-4xl transition-all ease-in-out`}
-            >
-              <div className="bars pt-4 pb-2 px-4">
-                <FaBars onClick={toggle} />
-              </div>
-              {menuItem.map((item, index) => (
-                <NavLink
-                  to={item.path}
-                  key={index}
-                  className="link flex p-3 hover:bg-gray-800 focus:bg-gray-900 items-center rounded"
-                  activeClassName="active"
-                >
-                  <div className="icon px-2">{item.icon}</div>
-                  <div
-                    style={{ display: isOpen ? "block" : "none" }}
-                    className="link_text text-base font-semibold"
-                  >
-                    {item.name}
-                  </div>
-                </NavLink>
-              ))}
-            </div>
-            <div
-              className={`main-content ${isOpen ? "ml-48" : "ml-16"
-                } transition-all ease-in-out flex-grow min-h-screen p-4`}
-            >
-              {children}
-            </div>
-          </div>
+    <div className="flex">
+      <div
+        className={`sidebar flex flex-col gap-3 text-white bg-gray-700 ${
+          isOpen ? "w-48" : "w-16"
+        } mt-2 ml-2 mb-2 rounded text-4xl transition-all ease-in-out`}
+      >
+        <div className="bars pt-4 pb-2 px-3">
+          <FaBars onClick={toggle} />
         </div>
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className="link flex p-3 hover:bg-gray-800 focus:bg-gray-900 items-center rounded"
+            activeClassName="active"
+          >
+            <div className="icon">{item.icon}</div>
+            <div
+              style={{ display: isOpen ? "block" : "none" }}
+              className="link_text pl-2 text-base font-semibold"
+            >
+              {item.name}
+            </div>
+          </NavLink>
+        ))}
+      </div>
+      <div
+        className={`main-content ${
+          isOpen ? "ml-48" : "ml-16"
+        } transition-all ease-in-out flex-grow min-h-screen p-4`}
+      >
+        {children}
       </div>
     </div>
   );
