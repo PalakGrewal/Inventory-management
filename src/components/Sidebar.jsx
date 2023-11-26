87f5bd5f158127e9be0b1119d13fee7bea440a42
Varsha Sharma <98809056+varshalearner@users.noreply.github.com>
2023-11-26 22:15:33 +0530

    sidebar fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -39,11 +39,10 @@ const Sidebar = ({ children }) => {
 
   return (
     <div
-      className="flex 
-    	min-height: 0px;
-    min-h-min h-screen text-4xl float-left"
+      className="flex
+     h-[90%] text-4xl float-left mr-4"
     >
-      <div className="sidebar flex sticky flex-col gap-3 text-white bg-gray-700 mt-2 ml-2 mb-2 rounded text-4xl ">
+      <div className="sidebar flex sticky flex-col text-white bg-gray-700 mt-2 ml-2 mb-2 rounded text-4xl ">
         <div className="bars pt-4  pb-2 px-4 ">
           <FaBars onClick={toggle} />
         </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,32 +1,20 @@
-<<<<<<< HEAD
 import React from "react";
 
 import Chart from "../components/Chart";
 import Kpi from "../components/Kpi/Kpi";
 import Table from "../components/Table";
-=======
-import React from 'react'
-import Sidebar from '../components/Sidebar'
-import Header from '../components/Header'
-import { Outlet } from 'react-router'
->>>>>>> 65e031ff6f33715943604349a11a43481266dfda
 
 const Dashboard = () => {
   return (
-    <div>
-<<<<<<< HEAD
+    <div className="p-8">
+
       <Kpi />
       <Chart/>
      
       <Table/>
     </div>
   );
-};
-=======
-      Dashboards
-    </div>
-  )
-}
->>>>>>> 65e031ff6f33715943604349a11a43481266dfda
+};    
+  
 
 export default Dashboard;
