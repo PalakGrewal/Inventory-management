80bdf3827ab493a7cfe06ee4b38bb40dff76847f
PalakGrewal <grewal.palak06@gmail.com>
2023-11-27 09:52:01 +0530

    table

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,56 +1,11 @@
-<<<<<<< HEAD
-import React from 'react'
-
+import React from 'react';
 const Inventory = () => {
     return (
         <section className="inventory">
-            
+            <input type="text" placeholder="Search Word" />
+            <button type="submit">Search</button>
         </section>
     )
 }
 
 export default Inventory
-
-=======
-import React from 'react'
-
-const Inventory = () => {
-    return (
-        <section className="inventory">
-            <table>
-                <tr>
-                    <td><a href="#">Stock List</a></td>
-                    <input type="text" placeholder="Search Word" />
-                    <button type="submit">Search</button>
-                    <thead>
-                        <tr>
-                            <th>Goods Code</th>
-                            <th>Goods Desc</th>
-                            <th>Total Qty</th>
-                            <th>On hand</th>
-                            <th>Can Order</th>
-                            <th>Ordered Stock</th>
-                            <th>Inspect</th>
-                            <th>Holding</th>
-                            <th>Damage</th>
-                            <th>ASN Stock</th>
-                            <th>DN Stock</th>
-                            <th>Pre Load</th>
-                            <th>Pre Sort</th>
-                            <th>Sorted Stock</th>
-                            <th>Pick Stock</th>
-                            <th>Picked Stock</th>
-                            <th>Back Order</th>
-                            <th>Cteate Time</th>
-                            <th>Update Time</th>
-                        </tr>
-                    </thead>
-                </tr>
-
-            </table>
-        </section>
-    )
-}
-
-export default Inventory
->>>>>>> 703a9730beac199f73cbcc466820a7c4203b5ca6
