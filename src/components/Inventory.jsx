02f2900d7a8350389b1ce041c50ba890cbf71d4f
Yatharth2609 <yatharth.mishra2002@gmail.com>
2023-12-01 00:30:25 +0530

    new

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -7,37 +7,42 @@ import { ListFilter } from "lucide-react";
 const Inventory = () => {
   return (
     <section className="h-[88vh] overflow-y-scroll p-10 w-full">
-    <section className="inventory h-[88vh] overflow-y-scroll p-10 w-full">
-      <div>
-      <h2 className="flex flex-1 justify-start text-4xl font-bold">Inventory Management</h2>
-      </div>
-      <div className="flex flex-1 mt-20">
-        <input
-          type="text"
-          placeholder="Search Word"
-          className="p-4 border-4 rounded border-color-black "
-        />
-        <button type="submit" className="text-2xl pl-3 ">Search</button>
-      </div>
-      <div>
-        <ul className="list-style-type: none padding-left: 0 mt-10">
-          <li className="mb-2 inline mr-8">
-            <ListFilter className="inline mr-2 w-4 h-4" />
-            Filter
-          </li>
-          <li className="mb-2 inline mr-8">
-            <FaPlus className="inline mr-1 w-4 h-4" />
-            New Item
-          </li>
-          <li className="mb-2 inline mr-8">
-            <FaPencil className="inline mr-2 w-4 h-4" />
-            Edit
-          </li>
-        </ul>
-      </div>
-      <div>
-        <TableInventory />
-      </div>
+      <section className="inventory h-[88vh] overflow-y-scroll p-10 w-full">
+        <div>
+          <h2 className="flex flex-1 justify-start text-4xl font-bold">
+            Inventory Management
+          </h2>
+        </div>
+        <div className="flex flex-1 mt-20">
+          <input
+            type="text"
+            placeholder="Search Word"
+            className="p-4 border-4 rounded border-color-black "
+          />
+          <button type="submit" className="text-2xl pl-3 ">
+            Search
+          </button>
+        </div>
+        <div>
+          <ul className="list-style-type: none padding-left: 0 mt-10">
+            <li className="mb-2 inline mr-8">
+              <ListFilter className="inline mr-2 w-4 h-4" />
+              Filter
+            </li>
+            <li className="mb-2 inline mr-8">
+              <FaPlus className="inline mr-1 w-4 h-4" />
+              New Item
+            </li>
+            <li className="mb-2 inline mr-8">
+              <FaPencil className="inline mr-2 w-4 h-4" />
+              Edit
+            </li>
+          </ul>
+        </div>
+        <div>
+          <TableInventory />
+        </div>
+      </section>
     </section>
   );
 };
