2410f94398977960da6185700f7a8ea2e9955b7e
MrtitaniumJ <jkjatinsharma72@gmail.com>
2023-12-01 00:03:22 +0530

    Product Context

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,67 +6,32 @@ const Profile = () => {
   const user = location.state?.user || {};
 
   return (
-    <div className="flex flex-col pl-9 pt-5 space-y-12 w-1/2">
-      <div className="border-b border-gray-900/10 pb-12">
-        <h2 className="text-base font-semibold leading-7 text-gray-900">
-          User Profile
-        </h2>
-
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div className="sm:col-span-4">
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Username
-            </label>
-            <div className="mt-2">
-              <p className="text-gray-900">{user.username}</p>
-            </div>
-          </div>
+    <div className="max-w-3xl mx-auto mt-10">
+      <div className="bg-white overflow-hidden shadow rounded-lg border">
+        <div className="px-4 py-5 sm:px-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">
+            User Profile
+          </h3>
+          <p className="mt-1 max-w-2xl text-sm text-gray-500">
+            This is some information about the user.
+          </p>
         </div>
-      </div>
-
-      <div className="border-b border-gray-900/10 pb-12">
-        <h2 className="text-base font-semibold leading-7 text-gray-900">
-          Personal Information
-        </h2>
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div className="sm:col-span-3">
-            <label
-              htmlFor="first-name"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              First name
-            </label>
-            <div className="mt-2">
-              <p className="text-gray-900">{user.firstName}</p>
+        <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+          <dl className="sm:divide-y sm:divide-gray-200">
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Full name</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {user.firstName} {user.lastName}
+              </dd>
             </div>
-          </div>
-
-          <div className="sm:col-span-3">
-            <label
-              htmlFor="last-name"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Last name
-            </label>
-            <div className="mt-2">
-              <p className="text-gray-900">{user.lastName}</p>
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Email address</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {user.email}
+              </dd>
             </div>
-          </div>
-
-          <div className="sm:col-span-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <p className="text-gray-900">{user.email}</p>
-            </div>
-          </div>
+            {/* Add more fields as needed */}
+          </dl>
         </div>
       </div>
 
