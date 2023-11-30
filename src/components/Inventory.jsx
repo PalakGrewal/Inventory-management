3d3e530a4241900d0c39a267a383b43e5360585a
PalakGrewal <grewal.palak06@gmail.com>
2023-11-30 22:17:32 +0530

    invent3

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { ListFilter } from 'lucide-react';
+import { ListFilter} from 'lucide-react';
+import { Plus } from 'lucide-react';
+import { Pencil } from 'lucide-react';
+import TableInventory from './TableInventory'
+
 const Inventory = () => {
   return (
     <section className="inventory">
@@ -8,95 +12,17 @@ const Inventory = () => {
         <button type="submit">Search</button>
       </div>
       <div>
-        <h2 className="text-xl font-semibold">
-          Inventory
+        <h2 className="text-xl font-semibold mt-16">
+          INVENTORY
         </h2>
-        <ul className="list-style-type: none
-  padding-left: 0">
-          <li className="mb-2">
-          <ListFilter />Filter
-          </li>
-          <li className="mb-2">
-            New Item
-          </li>
-          <li className="mb-2">
-            Edit
-          </li>
+        <ul className="list-style-type: none padding-left: 0 mt-10">
+          <li className="mb-2 inline mr-8"><ListFilter className="inline mr-2 w-4 h-4"/>Filter</li>
+          <li className="mb-2 inline mr-8"><Plus className="inline mr-1 w-4 h-4"/>New Item</li>
+          <li className="mb-2 inline mr-8"><Pencil className="inline mr-2 w-4 h-4"/>Edit</li>
         </ul>
       </div>
       <div>
-        <table className="w-full border-separate border-spacing-2 border-gray-200">
-          <thead
-            className="p-2
-  font-semibold
-  text-left
-  border-b border-gray-200"
-          >
-            <tr
-              className=":nth-child(even) {
-    background-color: #f3f4f6;
-  }"
-            >
-              <th
-                className="p-2
-  font-semibold
-  text-left
-  border-b border-gray-200"
-              >
-                Photo
-              </th>
-              <th
-                className="p-2
-  font-semibold
-  text-left
-  border-b border-gray-200"
-              >
-                Name
-              </th>
-              <th
-                className="p-2
-  font-semibold
-  text-left
-  border-b border-gray-200"
-              >
-                Purchasing Price
-              </th>
-              <th
-                className="p-2
-  font-semibold
-  text-left
-  border-b border-gray-200"
-              >
-                Selling Price
-              </th>
-              <th
-                className="p-2
-  font-semibold
-  text-left
-  border-b border-gray-200"
-              >
-                On-Hand Units
-              </th>
-              <th
-                className="p-2
-  font-semibold
-  text-left
-  border-b border-gray-200"
-              >
-                Unit
-              </th>
-              <th
-                className="p-2
-  font-semibold
-  text-left
-  border-b border-gray-200"
-              >
-                Updated
-              </th>
-            </tr>
-          </thead>
-          <tbody>{/* Table content goes here */}</tbody>
-        </table>
+          <TableInventory/>
       </div>
     </section>
   );
---
+++ src/components/TableInventory.jsx
@@ -0,0 +1,153 @@
+
+import H1 from "../Styles/H1";
+import Hgray from "../Styles/Hgray";
+
+const data = [
+  {
+    prod_image:
+      "https://dukaan.b-cdn.net/700x700/webp/786890/269f0e92-5de9-4b8c-aa2c-740359619486.png",
+    Name: "Air Conditioner",
+    pur_price: 20000,
+    sell_price: 30000,
+    onhand_units: "20",
+    sold_units:"50",
+    units: "pcs",
+    updated: "Yes",
+  },
+  {
+    prod_image:
+      "https://dukaan.b-cdn.net/700x700/webp/786890/269f0e92-5de9-4b8c-aa2c-740359619486.png",
+    Name: "Refrigerator",
+    pur_price: 15000,
+    sell_price: 20000,
+    onhand_units: "10",
+    sold_units:"30",
+    units: "pcs",
+    updated: "Yes",
+  },
+  {
+    prod_image:
+      "https://dukaan.b-cdn.net/700x700/webp/786890/269f0e92-5de9-4b8c-aa2c-740359619486.png",
+    Name: "Wall Decor Mirrors",
+    pur_price: 10,
+    sell_price: 20,
+    onhand_units: "",
+    sold_units:"",
+    units: "pcs",
+    updated: "",
+  },
+  {
+    prod_image:
+      "https://dukaan.b-cdn.net/700x700/webp/786890/269f0e92-5de9-4b8c-aa2c-740359619486.png",
+    Name: "20 Wall Posters",
+    pur_price: 10,
+    sell_price: 20,
+    onhand_units: "",
+    sold_units:"",
+    units: "pcs",
+    updated: "",
+  },
+  {
+    prod_image:
+      "https://dukaan.b-cdn.net/700x700/webp/786890/269f0e92-5de9-4b8c-aa2c-740359619486.png",
+    Name: "Item A",
+    pur_price: 10,
+    sell_price: 20,
+    onhand_units: "",
+    sold_units:"",
+    units: "pcs",
+    updated: "",
+  },
+  {
+    prod_image:
+      "https://dukaan.b-cdn.net/700x700/webp/786890/269f0e92-5de9-4b8c-aa2c-740359619486.png",
+    Name: "Item A",
+    pur_price: 10,
+    sell_price: 20,
+    onhand_units: "",
+    sold_units:"",
+    units: "pcs",
+    updated: "",
+  },
+  {
+    prod_image:
+      "https://dukaan.b-cdn.net/700x700/webp/786890/269f0e92-5de9-4b8c-aa2c-740359619486.png",
+    Name: "Item A",
+    pur_price: 10,
+    sell_price: 20,
+    onhand_units: "",
+    sold_units:"",
+    units: "pcs",
+    updated: "",
+  },
+  {
+    prod_image:
+      "https://dukaan.b-cdn.net/700x700/webp/786890/269f0e92-5de9-4b8c-aa2c-740359619486.png",
+    Name: "Item A",
+    pur_price: 10,
+    sell_price: 20,
+    onhand_units: "",
+    sold_units:"",
+    units: "pcs",
+    updated: "",
+  },
+];
+
+
+const TableInventory = () => {
+  return (
+    <div>
+      <H1 data="Inventory data"></H1>
+      <table className="pl-8 pt-8 w-max-80">
+        <thead>
+          <tr>
+            <td className="p-2">
+              <Hgray data="Product Image">Product Image</Hgray>
+            </td>
+            <td className="p-2">
+              <Hgray data="Item Name">Item Name</Hgray>
+            </td>
+            <td className="p-2">
+              <Hgray data="Purchasing Price">Purchasing Price</Hgray>
+            </td>
+            <td className="p-2">
+              <Hgray data="Selling Price">Selling Price</Hgray>
+            </td>
+            <td className="p-2">
+              <Hgray data="On-hand Units">On-hand Units</Hgray>
+            </td>
+            <td className="p-2">
+              <Hgray data="Sold Units">Sold Units</Hgray>
+            </td>
+            <td className="p-2">
+              <Hgray data="Units">Units</Hgray>
+            </td>
+            <td className="p-2">
+              <Hgray data="Updated">Updated</Hgray>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((value, key) => (
+            <tr key={key} className="hover:bg-gray-200 rounded-lg">
+              <td className="p-2">
+                <img
+                  className="rounded-lg"
+                  src={value.prod_image}
+                  alt={`Product ${key + 1}`}
+                  style={{ width: "50px", height: "50px" }}
+                />
+              </td>
+              <td className="p-2 text-base text-center">{value.name}</td>
+              <td className="p-2 text-base text-center">{value.itemsold}</td>
+              <td className="p-2 text-base text-center">{value.Onhand}</td>
+              <td className="p-2 text-base text-center">{value.units}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TableInventory;
